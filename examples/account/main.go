8ef674e51a4071f476d761428f9975decb2d0112
alfred-mk <alfredmwaik@gmail.com>
2024-08-09 19:55:30 +0300

    use the passed session -id

--- examples/account/main.go
+++ examples/account/main.go
@@ -18,8 +18,6 @@ import (
 	testdataloader "github.com/peteole/testdata-loader"
 )
 
-var globalSessionId string
-
 const (
 	USERFLAG_HASACCEPTED    = state.FLAG_USERSTART
 	USERFLAG_HASSESSION     = state.FLAG_USERSTART
@@ -27,7 +25,9 @@ const (
 	USERFLAG_ACCOUNTSUCCESS = state.FLAG_USERSTART
 	createAccountURL        = "https://custodial.sarafu.africa/api/account/create"
 	trackStatusURL          = "https://custodial.sarafu.africa/api/track/"
+)
 
+const (
 	StateNone = iota
 	StateAccountAccepted
 	StateTermsOffered
@@ -59,7 +59,6 @@ type trackStatusResponse struct {
 }
 
 type UserState struct {
-	PhoneNumber   string
 	CurrentState  int
 	PublicKey     string
 	CustodialId   string
@@ -78,13 +77,13 @@ type accountResource struct {
 	st *state.State
 }
 
-func saveUserState(phoneNumber string, state *UserState) error {
+func saveUserState(sessionId string, state *UserState) error {
 	data, err := json.Marshal(state)
 	if err != nil {
 		return err
 	}
 
-	fp := path.Join(scriptDir, phoneNumber+"_userstate.json")
+	fp := path.Join(scriptDir, sessionId+"_userstate.json")
 	err = os.WriteFile(fp, data, 0600)
 	if err != nil {
 		return err
@@ -92,8 +91,8 @@ func saveUserState(phoneNumber string, state *UserState) error {
 	return nil
 }
 
-func loadUserState(phoneNumber string) (*UserState, error) {
-	fp := path.Join(scriptDir, phoneNumber+"_userstate.json")
+func loadUserState(sessionId string) (*UserState, error) {
+	fp := path.Join(scriptDir, sessionId+"_userstate.json")
 	data, err := os.ReadFile(fp)
 	if err != nil {
 		return nil, err
@@ -108,18 +107,14 @@ func loadUserState(phoneNumber string) (*UserState, error) {
 	return &state, nil
 }
 
-func updateState(phoneNumber string, updates map[string]interface{}) error {
-	userState, err := loadUserState(phoneNumber)
+func updateState(sessionId string, updates map[string]interface{}) error {
+	userState, err := loadUserState(sessionId)
 	if err != nil {
 		return err
 	}
 
 	for key, value := range updates {
 		switch key {
-		case "PhoneNumber":
-			if v, ok := value.(string); ok {
-				userState.PhoneNumber = v
-			}
 		case "CurrentState":
 			if v, ok := value.(int); ok {
 				userState.CurrentState = v
@@ -140,44 +135,35 @@ func updateState(phoneNumber string, updates map[string]interface{}) error {
 			if v, ok := value.(string); ok {
 				userState.AccountStatus = v
 			}
-
 		default:
 			return fmt.Errorf("unknown field: %s", key)
 		}
 	}
 
-	return saveUserState(phoneNumber, userState)
+	return saveUserState(sessionId, userState)
 }
 
 func (ir *accountResource) check_session(ctx context.Context, sym string, input []byte) (resource.Result, error) {
 	var err error
 	state := &UserState{}
-	var phoneNumber = string(input)
 
-	globalSessionId = phoneNumber
+	sessionId := ctx.Value("SessionId").(string)
 
-	sessionFile, err := sessionExists(phoneNumber)
+	sessionFile, err := sessionExists(sessionId)
 	if err != nil {
 		return emptyResult, err
 	}
 
 	state.CurrentState = StateNone
-	state.PhoneNumber = phoneNumber
 
 	if sessionFile {
 		ir.st.SetFlag(USERFLAG_HASSESSION)
-
-		updates := map[string]interface{}{
-			"CurrentState": StateNone,
-		}
-
-		updateState(phoneNumber, updates)
 	} else if !sessionFile {
 		ir.st.ResetFlag(USERFLAG_HASSESSION)
 
-		path.Join(scriptDir, phoneNumber+"_userstate.json")
+		path.Join(scriptDir, sessionId+"_userstate.json")
 
-		saveUserState(phoneNumber, state)
+		saveUserState(sessionId, state)
 
 		return emptyResult, err
 	}
@@ -187,8 +173,8 @@ func (ir *accountResource) check_session(ctx context.Context, sym string, input 
 	}, err
 }
 
-func sessionExists(phoneNumber string) (bool, error) {
-	filePath := path.Join(scriptDir, phoneNumber+"_userstate.json")
+func sessionExists(sessionId string) (bool, error) {
+	filePath := path.Join(scriptDir, sessionId+"_userstate.json")
 	if _, err := os.Stat(filePath); err == nil {
 		return true, nil
 	} else if os.IsNotExist(err) {
@@ -200,18 +186,20 @@ func sessionExists(phoneNumber string) (bool, error) {
 
 func (ir *accountResource) accept_account(ctx context.Context, sym string, input []byte) (resource.Result, error) {
 	var err error
+	sessionId := ctx.Value("SessionId").(string)
 	updates := map[string]interface{}{
 		"CurrentState": StateAccountAccepted,
 	}
 
-	updateState(globalSessionId, updates)
+	updateState(sessionId, updates)
 
 	return emptyResult, err
 }
 
 func (ir *accountResource) accept_terms(ctx context.Context, sym string, input []byte) (resource.Result, error) {
 	var err error
-	state, err := loadUserState(globalSessionId)
+	sessionId := ctx.Value("SessionId").(string)
+	state, err := loadUserState(sessionId)
 
 	if err != nil {
 		return emptyResult, err
@@ -226,20 +214,22 @@ func (ir *accountResource) accept_terms(ctx context.Context, sym string, input [
 		}
 
 		updates := map[string]interface{}{
-			"CurrentState": StateAccountCreated,
+			"CurrentState": StateAccountCreationPending,
 			"PublicKey":    accountResp.Result.PublicKey,
 			"TrackingId":   accountResp.Result.TrackingId,
 			"CustodialId":  accountResp.Result.CustodialId.String(),
 		}
 
-		updateState(globalSessionId, updates)
+		updateState(sessionId, updates)
 	}
 
 	return emptyResult, err
 }
 
 func (ir *accountResource) checkIdentifier(ctx context.Context, sym string, input []byte) (resource.Result, error) {
-	state, err := loadUserState(globalSessionId)
+	sessionId := ctx.Value("SessionId").(string)
+
+	state, err := loadUserState(sessionId)
 	if err != nil {
 		return emptyResult, err
 	}
@@ -251,7 +241,8 @@ func (ir *accountResource) checkIdentifier(ctx context.Context, sym string, inpu
 }
 
 func (ir *accountResource) check_account_creation(ctx context.Context, sym string, input []byte) (resource.Result, error) {
-	state, err := loadUserState(globalSessionId)
+	sessionId := ctx.Value("SessionId").(string)
+	state, err := loadUserState(sessionId)
 
 	if err != nil {
 		return emptyResult, err
@@ -270,7 +261,8 @@ func (ir *accountResource) check_account_creation(ctx context.Context, sym strin
 }
 
 func (ir *accountResource) check_account_status(ctx context.Context, sym string, input []byte) (resource.Result, error) {
-	state, err := loadUserState(globalSessionId)
+	sessionId := ctx.Value("SessionId").(string)
+	state, err := loadUserState(sessionId)
 
 	if err != nil {
 		return emptyResult, err
@@ -282,16 +274,23 @@ func (ir *accountResource) check_account_status(ctx context.Context, sym string,
 		return emptyResult, err
 	}
 
-	updates := map[string]interface{}{
-		"AccountStatus": status,
-	}
-
-	updateState(globalSessionId, updates)
-
 	if status == "SUCCESS" {
 		ir.st.SetFlag(USERFLAG_ACCOUNTSUCCESS)
+
+		updates := map[string]interface{}{
+			"AccountStatus": status,
+			"CurrentState":  StateAccountCreated,
+		}
+
+		updateState(sessionId, updates)
 	} else if status == "REVERTED" {
 		ir.st.ResetFlag(USERFLAG_ACCOUNTSUCCESS)
+		updates := map[string]interface{}{
+			"AccountStatus": status,
+			"CurrentState":  StateAccountCreationPending,
+		}
+
+		updateState(sessionId, updates)
 		return emptyResult, err
 	}
 
@@ -367,11 +366,11 @@ func main() {
 	ca := cache.NewCache()
 	cfg := engine.Config{
 		Root:       "root",
-		SessionId:  globalSessionId,
+		SessionId:  sessionId,
 		OutputSize: uint32(size),
 	}
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, "SessionId", globalSessionId)
+	ctx = context.WithValue(ctx, "SessionId", sessionId)
 	en := engine.NewEngine(ctx, cfg, &st, rs, ca)
 	var err error
 	_, err = en.Init(ctx)
